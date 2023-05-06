7189b75423e9174022c18a25289e767bd7c5b45c
DDFranky22 <mattia@ncoreplat.com>
2023-05-06 14:36:53 +0200

    actually convert mjml and show

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,24 @@
 import express from 'express';
 import mjml2html from 'mjml';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: "*"
+}));
+app.use(express.json());
 const port = process.env.PORT;
 
-app.get('/internal-api/convert', (req, res) => {
+app.post('/internal-api/convert', (req, res) => {
     const mjmlCode = req.body.content;
     res.send(transformToHtml(mjmlCode));
 });
 
-
 app.listen(port, () => {
-    return console.log("Listening");
+    return console.log("Listening", port);
 });
 
 
